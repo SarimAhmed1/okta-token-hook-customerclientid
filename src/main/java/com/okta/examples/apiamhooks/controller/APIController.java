1ec6d60ff2d1e95a35198e44745b0991075e7302
Sarim Ahmed <82052108+SarimAhmed1@users.noreply.github.com>
2021-12-10 17:39:34 -0500

    update

--- src/main/java/com/okta/examples/apiamhooks/controller/APIController.java
+++ src/main/java/com/okta/examples/apiamhooks/controller/APIController.java
@@ -1,9 +1,6 @@
 package com.okta.examples.apiamhooks.controller;
 
-import com.okta.examples.apiamhooks.model.Beer;
-import com.okta.examples.apiamhooks.model.Person;
-import com.okta.examples.apiamhooks.repository.BeerRepository;
-import com.okta.examples.apiamhooks.repository.PersonRepository;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,70 +18,15 @@ import java.util.stream.Collectors;
 @RequestMapping("/api")
 public class APIController {
 
-    BeerRepository beerRepository;
-    PersonRepository personRepository;
 
-    public APIController(BeerRepository beerRepository, PersonRepository personRepository) {
-        this.beerRepository = beerRepository;
-        this.personRepository = personRepository;
+    @RequestMapping("/")
+    public String health() {
+        return "Service";
+    }
+    @RequestMapping("/health")
+    public String healthcheck() {
+        return "OK";
     }
 
-    @GetMapping("/good-beers")
-    public Collection<Beer> goodBeers() {
 
-        return beerRepository.findAll().stream()
-            .filter(this::isGreat)
-            .collect(Collectors.toList());
-    }
-
-    @GetMapping("/beers")
-    public Collection<Beer> beers() {
-        return beerRepository.findAll();
-    }
-
-    private boolean isGreat(Beer beer) {
-        return !beer.getName().equals("Budweiser") &&
-            !beer.getName().equals("Coors Light") &&
-            !beer.getName().equals("PBR");
-    }
-
-    @PostMapping("/add-beer")
-    @ResponseBody List<Beer> addBeer(@RequestBody Beer beer, @AuthenticationPrincipal Principal principal) {
-
-        beer = findOrCreateBeer(beer);
-        Person p = findOrCreatePerson(principal);
-
-        // no need to add if it's already there
-        if (!p.getFavoriteBeers().contains(beer)) {
-            p.getFavoriteBeers().add(beer);
-            personRepository.save(p);
-        }
-
-        return p.getFavoriteBeers();
-    }
-
-    @GetMapping("/me-beers")
-    @ResponseBody List<Beer> listBeers(@AuthenticationPrincipal Principal principal) {
-        return findOrCreatePerson(principal).getFavoriteBeers();
-    }
-
-    private Person findOrCreatePerson(Principal principal) {
-        Person p = personRepository.findByEmail(principal.getName());
-        if (p == null) {
-            p = new Person();
-            p.setEmail(principal.getName());
-            personRepository.save(p);
-        }
-        return p;
-    }
-
-    private Beer findOrCreateBeer(Beer beerToFind) {
-        Beer beer = beerRepository.findByName(beerToFind.getName());
-        if (beer == null) {
-            beer = new Beer();
-            beer.setName(beerToFind.getName());
-            beerRepository.save(beer);
-        }
-        return beer;
-    }
 }
--- src/main/java/com/okta/examples/apiamhooks/controller/HooksController.java
+++ src/main/java/com/okta/examples/apiamhooks/controller/HooksController.java
@@ -1,12 +1,9 @@
 package com.okta.examples.apiamhooks.controller;
 
-import com.okta.examples.apiamhooks.model.Beer;
-import com.okta.examples.apiamhooks.model.Person;
 import com.okta.examples.apiamhooks.model.hooks.IDTokenPatchResponse;
 import com.okta.examples.apiamhooks.model.hooks.TokenHookRequest;
 import com.okta.examples.apiamhooks.model.hooks.TokenHookResponse;
 import com.okta.examples.apiamhooks.model.hooks.TokenPatchResponse;
-import com.okta.examples.apiamhooks.repository.PersonRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,73 +16,22 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/hooks")
 public class HooksController {
 
-    private PersonRepository personRepository;
-
-    public HooksController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
-
     @PostMapping("/apiam")
     public TokenHookResponse apiam(@RequestBody TokenHookRequest request) {
         String login = request.getData().getContext().getUser().getProfile().getLogin();
-        Person person = personRepository.findByEmail(login);
 
         TokenHookResponse response = new TokenHookResponse();
-        if (person != null) {
-            IDTokenPatchResponse idTokenPatchResponse = new IDTokenPatchResponse();
-            idTokenPatchResponse.getValue().add(
-                new TokenPatchResponse.Value(
-                    "add", "/claims/beers", transformBeers(person.getFavoriteBeers())
-                )
-            );
-            response.getCommands().add(idTokenPatchResponse);
-        }
 
-        patchClientIpAddress(request, response);
-        patchCustomerId(request, response);
+        IDTokenPatchResponse idTokenPatchResponse = new IDTokenPatchResponse();
+        idTokenPatchResponse.getValue().add(
+            new TokenPatchResponse.Value(
+                "add", "/claims/beers", "hi"
+            )
+        );
+        response.getCommands().add(idTokenPatchResponse);
+
         
         return response;
     }
 
-    private void patchClientIpAddress(TokenHookRequest request, TokenHookResponse response) {
-        String ipAddress = request.getData().getContext().getRequest().getIpAddress();
-        if (ipAddress != null) {
-            IDTokenPatchResponse idTokenPatchResponse = new IDTokenPatchResponse();
-            idTokenPatchResponse.getValue().add(
-                new TokenPatchResponse.Value(
-                    "add", "/claims/client_ip_address", ipAddress
-                )
-            );
-            response.getCommands().add(idTokenPatchResponse);
-        }
-    }
-
-    private void patchCustomerId(TokenHookRequest request, TokenHookResponse response) {
-        String url = request.getData().getContext().getRequest().getUrl().getValue();
-        if (url != null) {
-            String[] pairs = url.split("&");
-            for (String pair : pairs) {
-                int idx = pair.indexOf("=");
-                String key = pair.substring(0, idx);
-                if ("customer_id".equals(key)) {
-                    String customerId = pair.substring(idx + 1);
-
-                    IDTokenPatchResponse idTokenPatchResponse = new IDTokenPatchResponse();
-            idTokenPatchResponse.getValue().add(
-                new TokenPatchResponse.Value(
-                    "add", "/claims/customer_id", customerId
-                )
-            );
-            response.getCommands().add(idTokenPatchResponse);
-
-                }
-            }
-            
-        }
-    }
-
-
-    private List<String> transformBeers(List<Beer> beers) {
-        return beers.stream().map(Beer::getName).collect(Collectors.toList());
-    }
 }
